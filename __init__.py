274330848e84e02d2ad1da5fada47ed4755335a6
Miranda, Mateus <104639136+ai4luc@users.noreply.github.com>
2025-02-10 14:51:26 -0300

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,22 @@
+"""
+ai4luc
+========
+
+About AI4LUC
+
+
+========
+Provides
+
+========
+How to use the package
+
+========
+Available modules
+
+
+"""
+from ._version import *
+from ai4luc.cerranet_v3 import cerranetv3_keras
+from ai4luc.smart_mask_labeling import main
+from ai4luc.utility import *
